bff8194fac48e66af7dd2ef9b1f8878f657c7efb
Harshit Diwan <118329805+AarshD16@users.noreply.github.com>
2025-09-22 22:14:41 +0530

    Update CTASection.tsx edit path for render deploy

--- apps/frontend/src/componet/template/blocks/CTASection.tsx
+++ apps/frontend/src/componet/template/blocks/CTASection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CTASection as CTASectionType } from "@ai/shared/types";
+import { CTASection as CTASectionType } from "../../../shared/types";
 
 type Props = CTASectionType;
 
