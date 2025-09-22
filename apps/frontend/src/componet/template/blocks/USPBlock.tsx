c1b936f62c3ae6c3ff5cd24941f935f8e921f005
Harshit Diwan <118329805+AarshD16@users.noreply.github.com>
2025-09-22 22:18:59 +0530

    Update USPBlock.tsx

--- apps/frontend/src/componet/template/blocks/USPBlock.tsx
+++ apps/frontend/src/componet/template/blocks/USPBlock.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { USPBlock as USPBlockType } from "../../../shared/types";
+import { USPBlock as USPBlockType } from "../../../../shared/types";
 
 type Props = USPBlockType;
 
